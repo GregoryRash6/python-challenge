b56c1ce2fc4e13b0e77ce2c17b6d1184bc5e7737
Gregory Rash <gregoryrash@Gregorys-MacBook-Pro.local>
2020-03-28 00:06:50 -0500

    add commented code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,9 @@
+# Import Dependencies
 import os
 import csv
-
+# Set Path for File
 file_location = '../../../nu-chi-data-pt-03-2020-u-c/Homework/03-Python/Instructions/PyBank/Resources/budget_data.csv'
-
+# Variables
 total_months = 0
 profit_loss = 0
 monthly_change = []
@@ -11,43 +12,43 @@ greatest_increase = 0
 greatest_increase_month = 0
 greatest_decrease = 0
 greatest_decrease_month = 0
-
+# Open and Read CSV
 with open(file_location, "r") as csvfile:
-    
+# Split Data on Commas   
     csvreader = csv.reader(csvfile, delimiter=',')
-
+# Read the Header
     csv_header = next(csvreader)
     row = next(csvreader)
-
+# Calculations
     previous_row = int(row[1])
     total_months += 1
     profit_loss += int(row[1])
     greatest_increase = int(row[1])
     greatest_increase_month = row[0]
-    
+# Read each Line after the Header   
     for row in csvreader:
-        
+#  Calculate Total Months       
         total_months += 1
         profit_loss += int(row[1])
-        
+# Calculate Revenue Chance        
         revenue_change = int(row[1]) - previous_row
         monthly_change.append(revenue_change)
         previous_row = int(row[1])
         month_count.append(row[0])
-        
+# Calculate Greatest Increase        
         if int(row[1]) > greatest_increase:
             greatest_increase = int(row[1])
             greatest_increase_month = row[0]
-        
+#Calculate Greatest Decrease 
         if int(row[1]) < greatest_decrease:
             greatest_decrease = int(row[1])
             greatest_decrease_month = row[0]
-    
+#  Calculate Average Change   
     average_change = sum(monthly_change) / len(monthly_change)
     
     high = max(monthly_change)
     low = min(monthly_change)
-
+# Print Financial Analysis
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
@@ -55,9 +56,9 @@ print(f"Total: ${profit_loss}")
 print(f"Average Change: ${average_change:.2f}")
 print(f"Greatest Increase in Profits: {greatest_increase_month} (${high})")
 print(f"Greatest Decrease in Profits: {greatest_decrease_month} (${low})")
-
+# Specify Output Location
 output_file_location = "PyBank"
-
+# Write Financial Analysis to Text File 
 with open(output_file_location, "w",) as txtfile:
     txtfile.write("Financial Analysis\n")
     txtfile.write("----------------------------\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,24 +1,25 @@
+# Import Dependencies
 import os
 import csv
-
+# Set Path for File
 file_location = '../../../nu-chi-data-pt-03-2020-u-c/Homework/03-Python/Instructions/PyPoll/Resources/election_data.csv'
-
+# Variables
 total = 0
 khan = 0
 correy = 0
 li = 0
 otooley = 0
-
+# Open and Read CSV
 with open(file_location, "r") as csvfile:
-
+# Spli Data on Commas
     csvreader = csv.reader(csvfile, delimiter=',')
-
+# Read the Header
     csv_header = next(csvreader)
-   
+# Read each Line after the Header   
     for row in csvreader:
-
+# Calculate Total Votes
         total += 1
-        
+#  Calculate Number of Votes for each Candidate               
         if (row[2] == "Khan"):
             khan += 1
         elif (row[2] == "Correy"):
@@ -27,14 +28,14 @@ with open(file_location, "r") as csvfile:
             li += 1
         elif (row[2] == "O'Tooley"):
             otooley += 1
-    
+# Calculate Percentage of Votes for each Candidate   
     khan_percent = khan / total
     correy_percent = correy / total
     li_percent = li / total
     otooley_percent = otooley /total
-
+# Find Candidate with the Most Votes
     election_winner = max(khan, correy, li, otooley)
-
+# Designate the Candidate with the Most Votes as Election Winner 
     if election_winner == khan:
         winner = "Khan"
     elif election_winner == correy:
@@ -43,7 +44,7 @@ with open(file_location, "r") as csvfile:
         winner = "Li"
     elif election_winner == otooley:
         winner = "O'Tooley"
-
+# Print Election Results 
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total}")
@@ -55,9 +56,9 @@ print(f"O'Tooley: {otooley_percent:.3%} ({otooley})")
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
-
+# Specify Output Location 
 output_file_location = "PyPoll"
-
+# Write Election Results to Text File
 with open(output_file_location, "w",) as txtfile:
     txtfile.write("Election Results\n")
     txtfile.write("-------------------------\n")
